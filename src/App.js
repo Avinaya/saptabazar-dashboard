ea3c194612960c89513a5bcfea6988c7c072863e
avinaya <avinayaacharya123@gmail.com>
2020-10-18 18:10:50 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MiniDrawer from './component/drawer/Drawer';
+
+const App = () => {
+  return (
+    <>
+      <MiniDrawer />
+    </>
+  );
+};
+export default App;
